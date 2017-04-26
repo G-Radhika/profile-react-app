86006bdf859052c8bf0c0fa73b62ad394bc0f5b8
Radhika Godugu <radhik.g@gmail.com>
2017-04-25 21:42:08 -0700

    App data loading: json file
    Signed-off-by: Radhika Godugu <radhik.g@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,9 +6,30 @@ import App from './App';
 import './css/index.css';
 import reducers from './reducers';
 
+let store=createStore(reducers)
+
+import jsonData from './data/profile.json'
+
+function loadData() {
+  store.dispatch({type : 'EDIT_BIODATA', payload: jsonData.BioData})
+  store.dispatch({type : 'ADD_SKILLS', payload: jsonData.Skills})
+  store.dispatch({type : 'ADD_PROJECTS', payload: jsonData.Projects})
+} 
+
+loadData()
+
+/*
+fetch('./data/profile.json')
+        .then(res => res.json())
+        .then(json =>  {
+          console.log('sending json data to store',json)
+          store.dispatch({type : 'ADD_BIODATA', payload: json.bioData})
+          store.dispatch({type : 'ADD_SKILLS', payload: json.skills})
+          store.dispatch({type : 'ADD_PROJECTS', payload: json.projects})
+       });*/
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
