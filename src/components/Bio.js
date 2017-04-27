689c98cb06b97f93b11589c6dd1bfe3ec12ea082
Radhika Godugu <radhik.g@gmail.com>
2017-04-27 13:17:35 -0700

    boostrap-stlying: look and feel
    Signed-off-by: Radhika Godugu <radhik.g@gmail.com>

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -21,14 +21,17 @@ class Bio extends Component {
         <div className="col col-md-3">
           <div className="card">
             <img className="card-img-top" src={bioData.photo} alt="Card image cap" />
+          <div className="card-block">
+            <p>{bioData.location}</p>
+          </div>
           </div>
         </div>
         <div className="col col-md-9">
           <h1 className="jumbotron-heading">{bioData.name}</h1>
           <p className="lead text-muted">{bioData.summary}</p>
           <p>
-            <a href="#" className="btn">LinkedIn</a>
-            <a href="#" className="btn">Email</a>
+            <a href={bioData.refLinkedin}  rel="noopener noreferrer" target="_blank" className="btn btn-primary btn-sm buttons-margin">LinkedIn</a>
+            <a href="#" rel="noopener noreferrer" target="_blank" className="btn btn-primary btn-sm buttons-margin">Email</a>
           </p>
         </div>
       </div>
